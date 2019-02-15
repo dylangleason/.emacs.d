317f0dc65728829d2130b44e3f57663e260c2fed
Dylan Gleason <dgleason8384@gmail.com>
2019-02-15 10:35:45 -0800

    Fix hooks for various c modes, add d-mode dependency

--- init/c-cpp.el
+++ init/c-cpp.el
@@ -1,3 +1,44 @@
+(defun rtags-setup-hook ()
+  (flycheck-select-checker 'rtags)
+  (rtags-start-process-unless-running)
+  (rtags-set-periodic-reparse-timeout 2.0))
+
+(defun maybe-c-make-project-hook ()
+  (when (file-exists-p "CMakeLists.txt")
+    (cmake-project-mode)))
+
+(defun my-c-mode-common-hook ()
+  (setq c-basic-offset 4
+        indent-tabs-mode nil
+        flycheck-mode t)
+  (c-set-offset 'case-label '+))
+
+(defun my-c-mode-hook ()
+  (c-set-style "k&r")
+  (set (make-local-variable 'compile-command) "cmake"))
+
+(defun my-c++-mode-hook ()
+  (c-set-style "stroustrup")
+  (set (make-local-variable 'compile-command) "cmake"))
+
+(defun my-d-mode-hook ()
+  (c-set-style "bsd")
+  (set (make-local-variable 'compile-command) "dmd")
+  (setq c-basic-offset 4))
+
+(add-hook 'c-mode-common-hook 'my-c-mode-common-hook)
+
+(add-hook 'c-mode-hook 'maybe-c-make-project-hook)
+(add-hook 'c-mode-hook 'my-c-mode-hook)
+(add-hook 'c-mode-hook 'rtags-setup-hook)
+
+(add-hook 'c++-mode-hook 'maybe-c-make-project-hook)
+(add-hook 'c++-mode-hook 'my-c++-mode-hook)
+(add-hook 'c++-mode-hook 'rtags-setup-hook)
+
+(use-package d-mode
+  :hook (d-mode . my-d-mode-hook))
+
 (use-package rtags
   :config
   (progn
@@ -23,35 +64,3 @@
   :after (flycheck rtags))
 
 (use-package cmake-project)
-
-(defun rtags-setup-hook ()
-  (flycheck-select-checker 'rtags)
-  (rtags-start-process-unless-running)
-  (rtags-set-periodic-reparse-timeout 2.0))
-
-(defun maybe-c-make-project-hook ()
-  (when (file-exists-p "CMakeLists.txt")
-    (cmake-project-mode)))
-
-;; c-mode hooks
-
-(defun my-c-mode-common-hook ()
-  (setq c-basic-offset 4
-        indent-tabs-mode nil
-        flycheck-mode t)
-  (c-set-offset 'case-label '+)
-  (set (make-local-variable 'compile-command) "cmake"))
-
-(add-hook 'c-mode-common-hook 'maybe-c-make-project-hook)
-(add-hook 'c-mode-common-hook 'my-c-mode-common-hook)
-(add-hook 'c-mode-common-hook 'rtags-setup-hook)
-
-;; c++-mode hooks
-
-(defun my-c++-mode-hook ()
-  (c-set-style "stroustrup")
-  (my-c-mode-common-hook))
-
-(add-hook 'c++-mode-hook 'maybe-c-make-project-hook)
-(add-hook 'c++-mode-hook 'my-c++-mode-hook)
-(add-hook 'c++-mode-hook 'rtags-setup-hook)
