ebbefcf857f44322135b598b3f0cded5d1e57001
Dylan Gleason <dgleason8384@gmail.com>
2019-12-04 22:00:22 -0800

    Modify scheme configs, use relative binary paths

--- init/scheme.el
+++ init/scheme.el
@@ -1,14 +1,23 @@
-(add-hook 'scheme-mode-hook 'my-lisp-mode-common-hook)
+(use-package scheme
+  :hook (scheme-mode . my-lisp-mode-common-hook))
 
 (use-package geiser
+  :after (scheme paredit)
   :defines (geiser-chez-binary
             geiser-guile-binary
             geiser-active-implementations
             geiser-repl-history-filename)
-  :after (scheme paredit)
   :hook (geiser-repl-mode . my-lisp-mode-common-hook)
   :init
-  (setq geiser-chez-binary "/usr/local/bin/chez"
-        geiser-guile-binary "/usr/local/bin/guile"
-        geiser-active-implementations '(chez chicken guile)
+  (setq geiser-chez-binary "chez"
+        geiser-guile-binary "guile"
+        geiser-active-implementations '(chez guile)
         geiser-repl-history-filename "~/.emacs.d/geiser-history"))
+
+(use-package ac-geiser
+  :after (geiser)
+  :hook ((geiser-mode . ac-geiser-setup)
+         (geiser-repl-mode . ac-geiser-setup))
+  :config
+  (eval-after-load "autocomplete"
+    '(add-to-list 'ac-modes 'geiser-repl-mode)))
