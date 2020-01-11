277a70efb53907331e6ebac6beb4be4713553c92
Dylan Gleason <dgleason8384@gmail.com>
2020-01-11 15:08:04 -0800

    Remove c-make-project deps, D configurations

--- init/c-cpp.el
+++ init/c-cpp.el
@@ -8,38 +8,25 @@
   (rtags-start-process-unless-running)
   (rtags-set-periodic-reparse-timeout 2.0))
 
-(defun maybe-c-make-project-hook ()
-  (when (file-exists-p "CMakeLists.txt")
-    (cmake-project-mode)))
-
 (defun my-c-mode-common-hook ()
-  (setq flycheck-mode t
-        indent-tabs-mode nil))
+  (flycheck-mode)
+  (setq-local compile-command "cmake")
+  (setq-local indent-tabs-mode nil))
 
 (defun my-c-mode-hook ()
-  (c-set-style "my-c-style")
-  (set (make-local-variable 'compile-command) "cmake"))
+  (c-set-style "my-c-style"))
 
 (defun my-c++-mode-hook ()
-  (c-set-style "my-c++-style")
-  (set (make-local-variable 'compile-command) "cmake"))
+  (c-set-style "my-c++-style"))
 
-(defun my-d-mode-hook ()
-  (c-set-style "my-d-style")
-  (set (make-local-variable 'compile-command) "dmd"))
-
-(setq my-c-style-alist '((c-basic-offset . 4)
-                         (c-offsets-alist (case-label . +))))
+(setq my-c-style-alist '((c-basic-offset . 4)))
 
 (c-add-style "my-c-style" (cons "k&r" my-c-style-alist))
 (c-add-style "my-c++-style" (cons "stroustrup" my-c-style-alist))
-(c-add-style "my-d-style" (cons "bsd" my-c-style-alist))
 
 (add-hook 'c-mode-common-hook 'my-c-mode-common-hook)
-(add-hook 'c-mode-hook 'maybe-c-make-project-hook)
 (add-hook 'c-mode-hook 'my-c-mode-hook)
 (add-hook 'c-mode-hook 'rtags-setup-hook)
-(add-hook 'c++-mode-hook 'maybe-c-make-project-hook)
 (add-hook 'c++-mode-hook 'my-c++-mode-hook)
 (add-hook 'c++-mode-hook 'rtags-setup-hook)
 
@@ -49,7 +36,6 @@
 (use-package rtags
   :defines rtags-use-helm
   :functions rtags-executable-find
-  :hook (kill-emacs . rtags-quit-rdm)
   :init
   (setq rtags-use-helm t)
   :config
@@ -65,9 +51,10 @@
 
 (use-package company-rtags
   :after (company rtags)
-  :init (setq rtags-completions-enabled t))
+  :init (setq rtags-completions-enabled t)
+  :config
+  (eval-after-load 'company-mode
+    (add-to-list 'company-backends 'company-rtags)))
 
 (use-package flycheck-rtags
   :after (flycheck rtags))
-
-(use-package cmake-project)
