84e123eff4edd27093d77ca0e76935ed80c5f481
Dylan Gleason <dgleason8384@gmail.com>
2020-01-19 13:38:28 -0800

    Add `cmake-mode', update rtags setup hook

--- init/c-cpp.el
+++ init/c-cpp.el
@@ -1,12 +1,15 @@
 (eval-when-compile
-  (defvar my-c-style-alist)
+  (defvar flycheck-check-syntax-automatically)
+  (defvar flycheck-highlighting-mode)
   (defvar flycheck-mode)
+  (defvar my-c-style-alist)
   (defun flycheck-select-checker (arg)))
 
-(defun rtags-setup-hook ()
+(defun my-rtags-setup-hook ()
   (flycheck-select-checker 'rtags)
-  (rtags-start-process-unless-running)
-  (rtags-set-periodic-reparse-timeout 2.0))
+  (rtags-set-periodic-reparse-timeout 2.0)
+  (setq-local flycheck-highlighting-mode nil)
+  (setq-local flycheck-check-syntax-automatically nil))
 
 (defun my-c-mode-common-hook ()
   (flycheck-mode)
@@ -20,18 +23,16 @@
   (c-set-style "my-c++-style"))
 
 (setq my-c-style-alist '((c-basic-offset . 4)))
-
 (c-add-style "my-c-style" (cons "k&r" my-c-style-alist))
 (c-add-style "my-c++-style" (cons "stroustrup" my-c-style-alist))
 
 (add-hook 'c-mode-common-hook 'my-c-mode-common-hook)
 (add-hook 'c-mode-hook 'my-c-mode-hook)
-(add-hook 'c-mode-hook 'rtags-setup-hook)
+(add-hook 'c-mode-hook 'my-rtags-setup-hook)
 (add-hook 'c++-mode-hook 'my-c++-mode-hook)
-(add-hook 'c++-mode-hook 'rtags-setup-hook)
+(add-hook 'c++-mode-hook 'my-rtags-setup-hook)
 
-(use-package d-mode
-  :hook (d-mode . my-d-mode-hook))
+(use-package cmake-mode)
 
 (use-package rtags
   :defines rtags-use-helm
@@ -47,11 +48,13 @@
 
 (use-package helm-rtags
   :after (helm rtags)
-  :init (setq rtags-display-result-backend 'helm))
+  :init
+  (setq rtags-display-result-backend 'helm))
 
 (use-package company-rtags
   :after (company rtags)
-  :init (setq rtags-completions-enabled t)
+  :init
+  (setq rtags-completions-enabled t)
   :config
   (eval-after-load 'company-mode
     (add-to-list 'company-backends 'company-rtags)))
