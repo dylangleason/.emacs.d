61ea6d2b4dd011f40df44a5ed45d353330351682
Dylan Gleason <dgleason8384@gmail.com>
2020-05-23 14:46:19 -0700

    Use elpy instead of lsp for Python development
    The lsp implementation for python-mode in Emacs appears to be to
    unstable for daily use.

--- init/init-python.el
+++ init/init-python.el
@@ -1,18 +1,32 @@
 (use-package python
-  :defines (flycheck-mode flycheck-python-pylint-executable)
-  :hook (python-mode . (lambda ()
-                         (flycheck-mode)
-                         (lsp)))
+  :defines (flycheck-mode
+            flycheck-disabled-checkers
+            flycheck-python-flake8-executable)
+  :hook (python-mode . flycheck-mode)
   :init
-  (setq flycheck-python-pylint-executable "~/.pyenv/shims/python")
+  (setq flycheck-python-flake8-executable "~/.pyenv/shims/python")
+  (setq-default flycheck-disabled-checkers '(python-pylint))
   :interpreter ("python" . python-mode)
   :mode ("\\.py\\'" . python-mode))
 
-(use-package hy-mode
-  :after (python)
-  :hook (hy-mode . my-lisp-mode-common-hook))
-
 (use-package py-test
   :after (python))
+
+(use-package elpy
+  :defer t
+  :init
+  (advice-add 'python-mode :before 'elpy-enable))
+
+(use-package pipenv
+  :defines (pipenv-projectile-after-switch-function)
+  :hook (python-mode . pipenv-mode)
+  :init
+  (setq pipenv-projectile-after-switch-function
+        #'pipenv-projectile-after-switch-extended))
+
+(use-package hy-mode
+  :after (python)
+  :hook (hy-mode . my-lisp-mode-common-hook)
+  :mode ("\\.hy\\'" . hy-mode))
 
 (provide 'init-python)
